f44a423db6a3a2dfb91d2684c06599fee7ce7e91
Rivenwu5 <2946996247@qq.com>
2024-08-07 15:20:36 +0800

    :apple:笔记.txt

---
+++ build/CMakeFiles/wrwSigSlot.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/riven/QT_sig_slot/src/MyWidget.cpp" "CMakeFiles/wrwSigSlot.dir/src/MyWidget.cpp.o" "gcc" "CMakeFiles/wrwSigSlot.dir/src/MyWidget.cpp.o.d"
+  "/home/riven/QT_sig_slot/src/main.cpp" "CMakeFiles/wrwSigSlot.dir/src/main.cpp.o" "gcc" "CMakeFiles/wrwSigSlot.dir/src/main.cpp.o.d"
+  "/home/riven/QT_sig_slot/src/moc_MyWidget.cpp" "CMakeFiles/wrwSigSlot.dir/src/moc_MyWidget.cpp.o" "gcc" "CMakeFiles/wrwSigSlot.dir/src/moc_MyWidget.cpp.o.d"
+  "/home/riven/QT_sig_slot/src/qobject.cpp" "CMakeFiles/wrwSigSlot.dir/src/qobject.cpp.o" "gcc" "CMakeFiles/wrwSigSlot.dir/src/qobject.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/wrwSigSlot.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for wrwSigSlot.
+# This may be replaced when dependencies are built.
